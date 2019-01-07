4c87345170e9ae50427c829bc63867189ead8251
anhnt97 <tuananh05031997@gmail.com>
2019-01-07 10:24:27 +0700

    Exercise 4.5 : Fix result slice use in place slice
    Fix result slice use in place slice in exercise 4.5

--- exercise-4-5.go
+++ exercise-4-5.go
@@ -8,9 +8,8 @@ func eliminateAdjacentDuplicates(strings []string) []string {
 	if len(strings) == 0 {
 		return strings
 	} else {
-		result := []string{}
+		result := strings[:1]
 		previous := strings[0]
-		result = append(result, previous)
 		for i, val := range strings {
 			if strings[i] != previous {
 				result = append(result, val)
@@ -20,15 +19,8 @@ func eliminateAdjacentDuplicates(strings []string) []string {
 		return result
 	}
 }
-func remove(slice []int, i int) []int {
-	// slice[i] = slice[len(slice)-1]
-	copy(slice[i:], slice[i+1:])
-	return slice[:len(slice)-1]
-}
 
 func main() {
-	// s := []int{5, 6, 7, 8, 9}
-	// result := remove(s, 2)
 	s := []string{"a", "a", "a", "b", "c", "c", "d", "a"}
 	result := eliminateAdjacentDuplicates(s)
 	fmt.Println(result)
