d340f81e20d037f33a3fb6baa328a6da9ba281fb
anhnt97 <tuananh05031997@gmail.com>
2019-01-07 08:42:16 +0700

    Exercise 4.11 : Fix result issues more than a year
     Fix result issues more than a year old when append result search issues

--- main-search-issues.go
+++ main-search-issues.go
@@ -26,18 +26,18 @@ func main() {
 		} else if days <= 365 {
 			resultsLessAYear.Items = append(resultsLessAYear.Items, item)
 		} else {
-			resultsLessAYear.Items = append(resultsMoreAYear.Items, item)
+			resultsMoreAYear.Items = append(resultsMoreAYear.Items, item)
 		}
 	}
-	fmt.Println("Issues less than a month old \n")
+	fmt.Println("Issues less than a month old : ")
 	for _, item := range resultsLessAMonth.Items {
 		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
 	}
-	fmt.Println("Issues less than a year old \n")
+	fmt.Println("Issues less than a year old : ")
 	for _, item := range resultsLessAYear.Items {
 		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
 	}
-	fmt.Println("Issues more than a year old \n")
+	fmt.Println("Issues more than a year old  :")
 	for _, item := range resultsMoreAYear.Items {
 		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
 	}
